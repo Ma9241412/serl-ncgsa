4e395029ca6aececbc01fb4722a7972ff5d0d216
shehrrbano <bscs23f11@namal.edu.pk>
2025-08-08 04:40:56 +0500

    About.css File Added

--- src/MainComponents/AboutComponents/AboutSection.jsx
+++ src/MainComponents/AboutComponents/AboutSection.jsx
@@ -1,39 +1,17 @@
 import React from "react";
-import mainbg from "../../assets/images/mainbg.jpg";
+import { Row, Col, Typography, Space } from "antd";
+import '../../Styles/About.css';
 
 const HeroSection = () => {
+  const { Title } = Typography;
   return (
-    <div
-      style={{
-        backgroundColor: "#000000",
-        minHeight: "80vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: "0",
-      }}
-    >
-      <div
-        style={{
-          backgroundImage: `url(${mainbg})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          width: "100%",
-          height: "80vh",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <div style={{ width: "100%", maxWidth: "1200px", display: "flex", alignItems: "center", justifyContent: "center", height: "100%" }}>
-          {/* width: '100%', maxWidth: '1200px' is the standard for all events and navigation bar width in Space Outreach section */}
-          <div style={{ width: "100%", maxWidth: "600px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-            <h1 style={{ color: "#FFFFFF", fontSize: "9.2rem", fontWeight: 600, margin: 0, fontFamily: "Inter", textAlign: "center" }}>ABOUT</h1>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Row className="about-bg" align="middle" justify="center" style={{ minHeight: '80vh', margin: 0 }}>
+      <Col xs={24} md={20} lg={16} xl={12} style={{ width: '100%' }}>
+        <Space direction="vertical" align="center" style={{ width: '100%' }}>
+          <Title className="about-title">ABOUT</Title>
+        </Space>
+      </Col>
+    </Row>
   );
 };
 
